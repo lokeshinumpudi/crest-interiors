4ebc7904d49b5818266257906e5915bc8988573f
Lokesh Inumpudi <inumpudi.lokesh@gmail.com>
2025-11-19 15:07:34 +0530

    feat: Remove Preloader component
    The Preloader component has been removed as it's no longer necessary for the application's loading flow. This simplifies the App component and removes unused code.

--- App.tsx
+++ App.tsx
@@ -12,7 +12,6 @@ import { ProjectDetail } from './components/ProjectDetail';
 import { CraftDetails } from './components/CraftDetails';
 import { WhatsAppButton } from './components/WhatsAppButton';
 import { FAQ } from './components/FAQ';
-import { Preloader } from './components/Preloader';
 import { ArtSettings } from './components/ArtSettings';
 import { PROJECTS } from './constants';
 import { AnimatePresence } from 'framer-motion';
@@ -33,64 +32,4 @@ const App: React.FC = () => {
   };
 
   const handleContact = () => {
-    isNavigatingToContact.current = true;
-    setActiveProjectId(null);
-    
-    setTimeout(() => {
-      const contactSection = document.getElementById('contact');
-      if (contactSection) {
-        contactSection.scrollIntoView({ behavior: 'smooth' });
-      }
-      setTimeout(() => {
-        isNavigatingToContact.current = false;
-      }, 1000);
-    }, 100);
-  };
-
-  useLayoutEffect(() => {
-    if (!activeProjectId && scrollPosition.current > 0 && !isNavigatingToContact.current) {
-      window.scrollTo({ top: scrollPosition.current, behavior: 'instant' });
-    }
-  }, [activeProjectId]);
-
-  const activeProject = activeProjectId ? PROJECTS.find(p => p.id === activeProjectId) : null;
-
-  return (
-    <ArtProvider>
-        <main className="w-full min-h-screen bg-stone-50 antialiased text-stone-900 selection:bg-bronze-500 selection:text-white relative">
-          <Preloader />
-          
-          <div className="grain" />
-          
-          <WhatsAppButton />
-          <ArtSettings />
-
-          <AnimatePresence mode="wait">
-            {activeProject ? (
-              <ProjectDetail 
-                key="project-detail" 
-                project={activeProject} 
-                onClose={handleClose} 
-                onContact={handleContact}
-              />
-            ) : (
-              <div key="home-view">
-                <Navbar />
-                <Hero />
-                <Projects onProjectSelect={handleProjectSelect} />
-                <CraftDetails />
-                <Services />
-                <Process />
-                <About />
-                <FAQ />
-                <Contact />
-                <Footer />
-              </div>
-            )}
-          </AnimatePresence>
-        </main>
-    </ArtProvider>
-  );
-};
-
-export default App;
+    isNavigatingToContact.current
